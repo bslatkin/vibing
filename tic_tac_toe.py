4fd1311b3ddd6110a8835ca9f07921484cbb5b8f
Brett Slatkin <brett@haxor.com>
2025-03-13 20:11:58 -0700

    Fixing block count

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -244,9 +244,9 @@ def generate_training_data():
 def create_transformer_model(
     sequence_length=9,
     embedding_dim=64,
-    num_heads=2,
+    num_heads=4,
     ff_dim=64,
-    num_transformer_blocks=4,
+    num_transformer_blocks=2,
 ):
     # Input: Sequence of board states
     board_input = keras.Input(
